dd65830e270ff34a38a1ef902384bce2b8468323
chenxizhou233 <chenxizhou2005@outlook.com>
2026-04-10 18:49:17 +0200

    Finished the migration from ch5

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -173,19 +173,29 @@ pub fn sys_sbrk(size: i32) -> isize {
 
 /// YOUR JOB: Implement spawn.
 /// HINT: fork + exec =/= spawn
-pub fn sys_spawn(_path: *const u8) -> isize {
+pub fn sys_spawn(path: *const u8) -> isize {
     trace!(
         "kernel:pid[{}] sys_spawn NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    let token = current_user_token();
+    let path = translated_str(token, path);
+    if let Some(data) = open_file(path.as_str(), OpenFlags::RDONLY) {
+        let task = current_task().unwrap();
+        let new_task = task.spawn(data.read_all().as_slice());
+        let new_pid = new_task.pid.0;
+        add_task(new_task);
+        new_pid as isize
+    } else {
+        -1
+    }
 }
 
 // YOUR JOB: Set task priority.
-pub fn sys_set_priority(_prio: isize) -> isize {
+pub fn sys_set_priority(prio: isize) -> isize {
     trace!(
         "kernel:pid[{}] sys_set_priority NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    current_task().unwrap().set_priority(prio)
 }
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -11,6 +11,8 @@ use alloc::vec;
 use alloc::vec::Vec;
 use core::cell::RefMut;
 
+pub const STRIDE: isize = 998244353;
+
 /// Task control block structure
 ///
 /// Directly save the contents that will not change during running
@@ -47,6 +49,32 @@ impl TaskControlBlock {
     pub fn current_munmap(&self, start: usize, len: usize) -> isize {
         self.inner_exclusive_access().memory_set.munmap(start, len)
     }
+
+    /// Set the priority
+    pub fn set_priority(&self, prio: isize) -> isize {
+        if prio < 2 {
+            return -1;
+        }
+        self.inner_exclusive_access().priority = prio;
+        prio
+    }
+
+    /// Get current stride
+    pub fn get_stride(&self) -> isize {
+        self.inner_exclusive_access().stride
+    }
+
+    /// Add delta to current stride
+    pub fn add_stride(&self, delta: isize) -> isize {
+        let mut inner = self.inner_exclusive_access();
+        inner.stride += delta;
+        inner.stride
+    }
+
+    ///Calculate the pass to be added to stride
+    pub fn pass(&self) -> isize {
+        STRIDE / self.inner_exclusive_access().priority
+    }
 }
 
 pub struct TaskControlBlockInner {
@@ -62,6 +90,12 @@ pub struct TaskControlBlockInner {
 
     /// Maintain the execution status of the current process
     pub task_status: TaskStatus,
+
+    /// The priority
+    pub priority: isize,
+
+    /// The "stride": the process of the task has run
+    pub stride: isize,
 
     /// Application address space
     pub memory_set: MemorySet,
@@ -130,6 +164,8 @@ impl TaskControlBlock {
                 UPSafeCell::new(TaskControlBlockInner {
                     trap_cx_ppn,
                     base_size: user_sp,
+                    priority: 16,
+                    stride: 0,
                     task_cx: TaskContext::goto_trap_return(kernel_stack_top),
                     task_status: TaskStatus::Ready,
                     memory_set,
@@ -218,6 +254,8 @@ impl TaskControlBlock {
                 UPSafeCell::new(TaskControlBlockInner {
                     trap_cx_ppn,
                     base_size: parent_inner.base_size,
+                    priority: 0,
+                    stride: 16,
                     task_cx: TaskContext::goto_trap_return(kernel_stack_top),
                     task_status: TaskStatus::Ready,
                     memory_set,
@@ -271,6 +309,19 @@ impl TaskControlBlock {
         } else {
             None
         }
+    }
+
+    /// Spawn, run a child programme without copying the memset
+    pub fn spawn(self: &Arc<Self>, elf_data: &[u8]) -> Arc<Self> {
+        let task_control_block = Arc::new(TaskControlBlock::new(elf_data));
+
+        // establish parent-child relationship
+        task_control_block.inner_exclusive_access().parent = Some(Arc::downgrade(self));
+        self.inner_exclusive_access()
+            .children
+            .push(task_control_block.clone());
+
+        task_control_block
     }
 }
 
